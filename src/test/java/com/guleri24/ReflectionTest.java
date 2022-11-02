a556a933a3df55d63881228000aea1e03ce3c5c7
Guleri24 <abhishekguleri24@gmail.com>
2022-11-02 12:30:26 +0530

    Bug fix in AttestationTest StorageElementIdentifier not found

--- src/test/java/com/guleri24/ReflectionTest.java
+++ src/test/java/com/guleri24/ReflectionTest.java
@@ -9,7 +9,7 @@ public class ReflectionTest {
     @Test
     public void reflectionTest() throws ClassNotFoundException {
         for (var method : TestClass.class.getDeclaredMethods()) {
-            System.out.println(String.format("Discovered method %s", method));
+            System.out.printf("Discovered method %s%n", method);
             try {
                 method.invoke(new TestClass(), new TestClass());
             } catch (IllegalAccessException | InvocationTargetException | IllegalArgumentException e) {
@@ -22,13 +22,13 @@ public class ReflectionTest {
         System.out.println(Class.forName(object.getClass().getName()));
     }
 
-    class TestClass {
+    static class TestClass {
 
         void methodOne() {
         }
 
         void methodTwo(TestClass arg) {
-            System.out.println(String.format("Argument: %s", arg));
+            System.out.printf("Argument: %s%n", arg);
         }
     }
 }
