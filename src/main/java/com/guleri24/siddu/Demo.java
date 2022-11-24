6840342acbe0a4147794a77547f1481d817ef672
Guleri24 <abhishekguleri24@gmail.com>
2022-11-24 22:49:37 +0530

    Added operation execution time to the Demo and DissertationDemo

--- src/main/java/com/guleri24/siddu/Demo.java
+++ src/main/java/com/guleri24/siddu/Demo.java
@@ -27,41 +27,66 @@ import java.util.logging.Logger;
 
 public class Demo {
     private final static String GREEN = "\u001B[32m";
+    private final static String CYAN = "\u001B[1;36m";
     private final static String BLUE = "\u001B[34m";
+
     private final static String RESET = "\u001B[0m";
 
     public static void main(String[] args) throws IOException, CloneNotSupportedException {
+        OperationTime time = new OperationTime();
+
         // Generating the storage layer.
         System.out.println("Running...");
+
+        time.start();
         var storageLayer = new MultiMappedStorageLayer();
+        time.end();
+        System.out.println(CYAN + "Storage Layer Setup Time: " + time.getDuration() + "ms" + RESET);
+
+        time.start();
         var macaroonManager = new APILayerMacaroonManager();
+        time.end();
+        System.out.println(CYAN + "Macaroon Manager Setup Time: " + time.getDuration() + "ms" + RESET);
 
         // Generating encryption keys for the cloud storage service providers.
+
+        time.start();
         var cloudA = EntityIdentifier.generateEntityIdentifierPair("cloudA");
         var cloudB = EntityIdentifier.generateEntityIdentifierPair("cloudB");
         var cloudC = EntityIdentifier.generateEntityIdentifierPair("cloudC");
         var cloudD = EntityIdentifier.generateEntityIdentifierPair("cloudD");
+        time.end();
+        System.out.println(CYAN + "Cloud Encryption Key Generation Time: " + time.getDuration() + "ms" + RESET);
 
+        time.start();
         List<Pair<String, Pair<PrivateEntityIdentifier, PublicEntityIdentifier>>> clouds = new ArrayList<>();
         clouds.add(new ImmutablePair<>("cloudA", cloudA));
         clouds.add(new ImmutablePair<>("cloudB", cloudB));
         clouds.add(new ImmutablePair<>("cloudC", cloudC));
         clouds.add(new ImmutablePair<>("cloudD", cloudD));
+        time.end();
+        System.out.println(CYAN + "Clouds List Setup Time: " + time.getDuration() + "ms" + RESET);
 
 
         // Generating encryption keys for the users.
+        time.start();
         var userA = EntityIdentifier.generateEntityIdentifierPair("userA");
         var userB = EntityIdentifier.generateEntityIdentifierPair("userB");
         var userC = EntityIdentifier.generateEntityIdentifierPair("userC");
         var userD = EntityIdentifier.generateEntityIdentifierPair("userD");
+        time.end();
+        System.out.println(CYAN + "User Encryption Key Generation Time: " + time.getDuration() + "ms" + RESET);
 
+        time.start();
         List<Pair<String, Pair<PrivateEntityIdentifier, PublicEntityIdentifier>>> users = new ArrayList<>();
         users.add(new ImmutablePair<>("userA", userA));
         users.add(new ImmutablePair<>("userB", userB));
         users.add(new ImmutablePair<>("userC", userC));
         users.add(new ImmutablePair<>("userD", userD));
+        time.end();
+        System.out.println(CYAN + "Users List Setup Time: " + time.getDuration() + "ms" + RESET);
 
-
+        time.start();
         List<Pair<String, Pair<PrivateEntityIdentifier, PublicEntityIdentifier>>> allEntities = new ArrayList<>();
         allEntities.addAll(clouds);
         allEntities.addAll(users);
@@ -73,13 +98,19 @@ public class Demo {
                     GREEN, RESET, entity.getRight().getLeft().getIBEIdentifier(),
                     GREEN, RESET, entity.getRight().getRight().getNamespaceServiceProviderEmailAddressUserConcatenation());
         }
+        time.end();
+        System.out.println(CYAN + "All Entities List Setup Time: " + time.getDuration() + "ms" + RESET);
 
+        time.start();
         Map<PublicEntityIdentifier, StorageElementIdentifier> queueStorageElementIdentifiers = new HashMap<>();
         queueStorageElementIdentifiers.put(userA.getRight(), new StorageElementIdentifier(userA.getRight().getNamespaceServiceProviderEmailAddressUserConcatenation()));
         queueStorageElementIdentifiers.put(userB.getRight(), new StorageElementIdentifier(userB.getRight().getNamespaceServiceProviderEmailAddressUserConcatenation()));
         queueStorageElementIdentifiers.put(userC.getRight(), new StorageElementIdentifier(userC.getRight().getNamespaceServiceProviderEmailAddressUserConcatenation()));
+        time.end();
+        System.out.println(CYAN + "Queue Storage Element Identifiers Setup Time: " + time.getDuration() + "ms" + RESET);
 
         // Registering the users to the respective cloud storage service providers.
+        time.start();
         NamespaceAttestation namespaceAttestationUserC = null;
         for (var i = 0; i < 3; i++) {
             var user = users.get(i);
@@ -96,9 +127,12 @@ public class Demo {
             if (i == 2) namespaceAttestationUserC = namespaceAttestation;
             System.out.printf("%nFirst attestation in personal queue of %s%s%s:\t%s%n", BLUE, user.getLeft(), RESET, namespaceAttestation);
         }
+        time.end();
+        System.out.println(CYAN + "Registering the users to the respective cloud storage service providers: " + time.getDuration() + "ms" + RESET);
         System.out.println();
 
         // Generating the necessary attestations for the demo.
+        time.start();
         var policy = new RTreePolicy(PolicyRight.WRITE, "userA", "file");
         List<Pair<Pair<PrivateEntityIdentifier, PublicEntityIdentifier>, Pair<PrivateEntityIdentifier, PublicEntityIdentifier>>>
                 issuersReceiversAttestationsDemo = new ArrayList<>();
@@ -125,8 +159,11 @@ public class Demo {
             generatedAttestationsForDemo.add(attestation);
             System.out.printf("%sAttestation (%s --> %s):%s\t%s%n", BLUE, entityNames.remove(0), entityNames.remove(0), RESET, attestation);
         }
+        time.end();
+        System.out.println(CYAN + "Generating the necessary attestations for the demo: " + time.getDuration() + "ms" + RESET);
 
         // Personal queues
+        time.start();
         entityNames.add("userA");
         entityNames.add("userB");
         entityNames.add("userC");
@@ -141,17 +178,23 @@ public class Demo {
                 }
             }
         }
+        time.end();
+        System.out.println(CYAN + "Personal queues: " + time.getDuration() + "ms" + RESET);
 
 
         // Obtain the first ephemeral AES key of the namespace attestation of user C.
+        time.start();
         var aesEncryptionInformationSegmentNamespaceAttestationUserC =
                 namespaceAttestationUserC.getFirstLayer().getAesEncryptionInformationSegment();
         var aesKeys = aesEncryptionInformationSegmentNamespaceAttestationUserC.decrypt(userC.getLeft(), new RTreePolicy(PolicyRight.WRITE, "userC"));
         var firstEphemeralAESKeyNamespaceAttestationUserC = aesKeys.getAesKeyInformation().getLeft();
         aesKeys = generatedAttestationsForDemo.get(2).getFirstLayer().getAesEncryptionInformationSegment()
                 .decrypt(userC.getLeft(), policy);
+        time.end();
+        System.out.println(CYAN + "Obtain the first ephemeral AES key of the namespace attestation of user C: " + time.getDuration() + "ms" + RESET);
 
         // Generating the first proof object.
+        time.start();
         System.out.printf("%n%sGenerating first variant of the proof object (userA --> userB --> userC):%s%n", BLUE, RESET);
         Logger.getLogger(ProofObject.class.getName()).setLevel(Level.ALL);
         var proofObjectOne = ProofObject.generateProofObject(generatedAttestationsForDemo.get(2), aesKeys.getAesKeyInformation().getLeft(),
@@ -159,8 +202,11 @@ public class Demo {
         System.out.printf("%sResulting proof object:%s%n%s%n", BLUE, RESET, proofObjectOne);
         var macaroonOne = macaroonManager.registerPolicy(proofObjectOne.verify(storageLayer));
         System.out.printf("%sResulting macaroon:%s%n%s%n%n", BLUE, RESET, macaroonOne);
+        time.end();
+        System.out.println(CYAN + "Generating the first proof object: " + time.getDuration() + "ms" + RESET);
 
         // Generating the second proof object.
+        time.start();
         aesKeys = generatedAttestationsForDemo.get(1).getFirstLayer().getAesEncryptionInformationSegment()
                 .decrypt(userC.getLeft(), policy);
         System.out.printf("%sGenerating second variant of the proof object (userA --> userC):%s%n", BLUE, RESET);
@@ -169,13 +215,19 @@ public class Demo {
         System.out.printf("%sResulting proof object:%s%n%s%n", BLUE, RESET, proofObjectTwo);
         var macaroonTwo = macaroonManager.registerPolicy(proofObjectTwo.verify(storageLayer));
         System.out.printf("%sResulting macaroon:%s%n%s%n%n", BLUE, RESET, macaroonTwo);
+        time.end();
+        System.out.println(CYAN + "Generating the second proof object: " + time.getDuration() + "ms" + RESET);
 
         // Revoking the attestation user A --> user B
+        time.start();
         var revocationObject = new RevocationObject(new RevocationCommitment(revocationSecrets.get(0)), revocationSecrets.get(0));
         System.out.printf("%sRevoking the attestation (userA --> userB):%s%n%s%n%n", BLUE, RESET, revocationObject);
         storageLayer.put(revocationObject);
+        time.end();
+        System.out.println(CYAN + "Revoking the attestation user A --> user B: " + time.getDuration() + "ms" + RESET);
 
         // Generating the second proof object.
+        time.start();
         aesKeys = generatedAttestationsForDemo.get(1).getFirstLayer().getAesEncryptionInformationSegment()
                 .decrypt(userC.getLeft(), policy);
         System.out.printf("%sGenerating second variant of the proof object (userA --> userC):%s%n", BLUE, RESET);
@@ -184,8 +236,11 @@ public class Demo {
         System.out.printf("%sResulting proof object:%s%n%s%n", BLUE, RESET, proofObjectTwo);
         macaroonTwo = macaroonManager.registerPolicy(proofObjectTwo.verify(storageLayer));
         System.out.printf("%sResulting macaroon:%s%n%s%n%n", BLUE, RESET, macaroonTwo);
+        time.end();
+        System.out.println(CYAN + "Generating the second proof object: " + time.getDuration() + "ms" + RESET);
 
         // Generating the first proof object.
+        time.start();
         System.out.printf("%n%sGenerating first variant of the proof object (userA --> userB --> userC):%s%n", BLUE, RESET);
         Logger.getLogger(ProofObject.class.getName()).setLevel(Level.ALL);
         aesKeys = generatedAttestationsForDemo.get(2).getFirstLayer().getAesEncryptionInformationSegment()
@@ -200,6 +255,8 @@ public class Demo {
             System.out.printf("%sAn exception is thrown... as expected!%s%n", GREEN, RESET);
             e.printStackTrace();
         }
+        time.end();
+        System.out.println(CYAN + "Generating the first proof object: " + time.getDuration() + "ms" + RESET);
 
     }
 }
